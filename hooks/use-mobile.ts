3ebc642cb542d0f8510b4edad5121d11231a1046
Jose Sanchez <engineer@copydataai.com>
2026-02-04 00:29:16 +0000

    chore(hooks): add use-mobile

---
+++ hooks/use-mobile.ts
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+
+    const update = () => setIsMobile(mediaQuery.matches);
+    update();
+
+    if (mediaQuery.addEventListener) {
+      mediaQuery.addEventListener("change", update);
+      return () => mediaQuery.removeEventListener("change", update);
+    }
+
+    mediaQuery.addListener(update);
+    return () => mediaQuery.removeListener(update);
+  }, []);
+
+  return isMobile;
+}
