5fb9ccf7bdf45a3827ece5df99af6e4a6523e7da
Jose Sanchez <engineer@copydataai.com>
2026-02-05 13:43:06 +0000

    chore(customers): add center components import

---
+++ components/customers/index.ts
@@ -0,0 +1,6 @@
+export { default as CustomerCard } from "./CustomerCard";
+export { default as CustomerForm } from "./CustomerForm";
+export { default as CustomerStatusBadge } from "./CustomerStatusBadge";
+export { default as CustomerBookingHistory } from "./CustomerBookingHistory";
+export { default as CustomerQuoteHistory } from "./CustomerQuoteHistory";
+export { default as CustomerPaymentInfo } from "./CustomerPaymentInfo";
